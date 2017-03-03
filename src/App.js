a779d92f44c87b63d8a51ea8663514ec2862e69b
Andrew Murray <sckoobs@gmail.com>
2017-03-03 16:54:58 +0000

    Adding some more routes and components to start fleshing out screens

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+import TabBar from './components/TabBar';
 import TopBar from './components/TopBar';
+import Dashboard from './components/Dashboard';
+import PlanList from './components/PlanList';
+import PlanEdit from './components/PlanEdit';
 
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <TopBar title="Lamps" />
-        
+        <Router>
+          <TopBar title="Lamps" />
+          <TabBar />
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/plans" component={PlanList} />
+          <Route path="/plans/:planId" component={PlanEdit} />
+        </Router>
       </MuiThemeProvider>
     );
   }
 }
-
-export default App;
---
+++ src/components/PlanEdit.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class PlanEdit extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
